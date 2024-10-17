a562a4e7dc9045a1fb3146e1f070257a0c863b34
skiiipy69 <122540427+skiiipy69@users.noreply.github.com>
2024-10-17 17:06:03 +0100

    Update LoginForm.jsx

--- src/components/Form/LoginForm.jsx
+++ src/components/Form/LoginForm.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Link, Form, useActionData } from "react-router-dom";
-import { Loader2 } from "lucide-react";
 
 function LoginForm({ loading }) {
   const actionData = useActionData();
@@ -79,7 +78,10 @@ function LoginForm({ loading }) {
       >
         {loading ? (
           <>
-            <Loader2 className="w-5 h-5 mr-2 animate-spin" />
+            <svg className="animate-spin h-5 w-5 mr-3 text-white" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+            </svg>
             Signing In...
           </>
         ) : (
