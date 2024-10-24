4312d1e535f0095ecf8bb1bf1a368c2c2443eee9
DadMed <mohammed.dad.ens@gmail.com>
2024-10-24 16:40:50 +0100

    updating options

--- src/components/Option/Option.jsx
+++ src/components/Option/Option.jsx
@@ -13,8 +13,8 @@ function Option({ value, idx, handleClick, trueAnswer, userAnswer, summary }) {
           }
           : {}
       }
-      className={`flex items-center space-x-3 mb-5 text-neutral-600 bg-neutral-200/50 rounded-full py-3 px-3  text-xs md:text-sm active:text-neutral-50 active:bg-orange-500/90  ${!summary &&
-        "md:hover:bg-orange-500/90 md:hover:text-neutral-50 cursor-pointer"
+      className={`flex items-center space-x-3 mb-5 text-neutral-600 bg-neutral-200/50 rounded-full py-3 px-3  text-xs md:text-sm active:text-neutral-50 active:bg-purple-500/90  ${!summary &&
+        "md:hover:bg-teal-500/90 md:hover:text-neutral-50 cursor-pointer"
         } ${trueAnswer === value && summary
           ? "bg-green-200 text-green-800 font-semibold"
           : "font-medium "
