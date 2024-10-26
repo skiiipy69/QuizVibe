b60d06e711c8038489cf1aaf12d80a10952ef362
DadMed <mohammed.dad.ens@gmail.com>
2024-10-26 01:00:52 +0100

    removing the las update

--- src/pages/Success/Success.jsx
+++ src/pages/Success/Success.jsx
@@ -23,9 +23,8 @@ function Success() {
 
   const handleClick = () => {
     resetQuestion();
-    navigate("/dashboard"); // Updated to navigate to dashboard
+    navigate("/");
   };
-
   return (
     <AnimateProvider className="flex flex-col space-y-10 md:max-w-xl md:mx-auto">
       <h3 className="text-lg text-center text-neutral-900 font-bold md:text-xl">
